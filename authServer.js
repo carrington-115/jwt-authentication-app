4d30e0a0876fda4802c2ef99f49c30c54af4cfb6
Mark Carrington <frumarkcarrington@gmail.com>
2024-07-17 16:07:25 +0530

    added the /token post route for to automate the creation of the refresh token using the access token

--- authServer.js
+++ authServer.js
@@ -4,12 +4,25 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
+const refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const { token } = req.body;
+  if (token === null) return res.sendStatus(401);
+  if (!refreshTokens.includes(token)) return res.sendStatus(403);
+  jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
 
 app.post("/login", (req, res) => {
   const { username } = req.body;
   const user = { name: username };
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
   res
     .status(200)
     .json({ accessToken: accessToken, refreshToken: refreshToken });
