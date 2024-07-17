552f1f43a5153bf75803d59da5382059011a51c4
Mark Carrington <frumarkcarrington@gmail.com>
2024-07-17 10:29:55 +0530

    wrote the function to make the access token temporal returned the refresh and access token in the server response

--- authServer.js
+++ authServer.js
@@ -1,9 +1,26 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+app.use(express.json());
 
 app.post("/login", (req, res) => {
   const { username } = req.body;
   const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.status(200).json({ accessToken: accessToken });
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  res
+    .status(200)
+    .json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20s" });
+};
+
+app.listen(process.env.AUTH_SERVER_PORT, () => {
+  console.log(
+    `The auth server is running on port: ${process.env.AUTH_SERVER_PORT}`
+  );
 });
